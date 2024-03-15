04b24983f4d747438681c4e73dbc7685e877dfdd
Alex McHale <amchale@greenarrowemail.com>
2024-03-15 13:29:28 -0400

    Add comments

--- 04.treenode/package.go
+++ 04.treenode/package.go
@@ -10,13 +10,17 @@ func NewTreeNode(key string, value int) *TreeNode {
 }
 
 func (node *TreeNode) Equal(other *TreeNode) bool {
+	// TreeNodes are considered equal if their keys are equal.
 	return false
 }
 
 func (node *TreeNode) AddChild(key string, value int) *TreeNode {
+	// Add the child with the given key to this +node+. Each TreeNode may
+	// have up to one direct child with a given key.
 	return &TreeNode{}
 }
 
 func (node *TreeNode) GetChild(key string) *TreeNode {
+	// Find and return a direct child of +node+. Do not look down the tree.
 	return nil
 }
